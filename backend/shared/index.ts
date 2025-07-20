568fe16353b36daec8aa5ff1f4e5ae9d678aa3af
danny bengal <febrero.daniel@gmail.com>
2025-07-20 15:06:08 +0200

    ok

---
+++ backend/shared/index.ts
@@ -0,0 +1,14 @@
+// Main exports for the shared layer
+export * from "./types";
+export * from "./types/user";
+export * from "./utils/dynamodb";
+export * from "./utils/s3";
+export * from "./utils/response";
+export * from "./utils/admin";
+export * from "./utils/user";
+export * from "./utils/thumbnail";
+export * from "./utils/email";
+export * from "./utils/parameters";
+export * from "./utils/revalidation";
+export * from "./auth/admin-middleware";
+export * from "./auth/user-middleware";
