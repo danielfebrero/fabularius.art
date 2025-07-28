e4b14316a77ea1991eaf833f45c799a12d776702
danny bengal <febrero.daniel@gmail.com>
2025-07-28 19:26:20 +0200

    Fix margin on maincontentwrapper

--- frontend/src/components/MainContentWrapper.tsx
+++ frontend/src/components/MainContentWrapper.tsx
@@ -24,6 +24,8 @@ export function MainContentWrapper({ children }: MainContentWrapperProps) {
 
   // Default: apply padding for discover page, albums, etc.
   return (
-    <main className="container mx-auto px-4 md:py-8 flex-1">{children}</main>
+    <main className="container mx-auto px-4 py-4 md:py-8 flex-1">
+      {children}
+    </main>
   );
 }
